87aa807d743bf86e5c8491bcbfac66f96e0c7156
Ananya R Siri <ananyasiri615@gmail.com>
2023-10-03 09:58:40 +0530

    backend code commit 1

---
+++ Test-Paper-Generator/src/main/java/com/tpg/TestPaperGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.tpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPaperGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPaperGeneratorApplication.class, args);
+	}
+
+}
