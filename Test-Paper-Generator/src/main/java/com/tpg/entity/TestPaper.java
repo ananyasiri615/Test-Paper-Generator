6ae8bbd91e7d766a67d935db21385a248c0d1cdc
Ananya R Siri <ananyasiri615@gmail.com>
2023-10-05 11:24:30 +0530

    Questions Done

--- Test-Paper-Generator/src/main/java/com/tpg/entity/TestPaper.java
+++ Test-Paper-Generator/src/main/java/com/tpg/entity/TestPaper.java
@@ -2,13 +2,13 @@ package com.tpg.entity;
 
 import java.util.Date;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "testpaper")
--- Test-Paper-Generator/src/main/java/com/tpg/service/QuestionsService.java
+++ Test-Paper-Generator/src/main/java/com/tpg/service/QuestionsService.java
@@ -1,5 +1,57 @@
 package com.tpg.service;
 
-public class QuestionsService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.tpg.entity.Questions;
+import com.tpg.entity.Subject;
+import com.tpg.repository.QuestionsRepository;
+
+@Service
+public class QuestionsService {
+	
+	@Autowired
+    private QuestionsRepository questionsRepository;
+	
+	@Autowired
+    private SubjectService subjectService; 
+
+    public Iterable<Questions> getAllQuestions() {
+        return questionsRepository.findAll();
+    }
+    
+    public Questions findById(int q_id) {
+        return questionsRepository.findById(q_id).orElse(null);
+    }
+    
+    public Questions createQuestions(@RequestBody Questions questions) {
+    	int subject_id = questions.getSubject().getSubject_id(); 
+        Subject subject = subjectService.findById(subject_id); 
+        questions.setSubject(subject);
+        return questionsRepository.save(questions);
+    }
+    
+    public Questions updateQuestions(@PathVariable int q_id, @RequestBody Questions questions) {
+        questions.setQ_id(q_id);
+        int subject_id = questions.getSubject().getSubject_id(); 
+        Subject subject = subjectService.findById(subject_id); 
+        questions.setSubject(subject);
+        return questionsRepository.save(questions);
+    }
+    
+    public void deleteQuestions(@PathVariable int q_id) {
+		Optional<Questions> questions = questionsRepository.findById(q_id);
+        if (questions.isPresent()) { 
+        	questionsRepository.deleteById(q_id);
+        }
+    }
+
+    public List<Questions> findAll() {
+        return questionsRepository.findAll();
+    }
 }
