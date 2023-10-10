5ae25bc64fb53b18e68350f45d53b392ba98d55e
Ananya R Siri <ananyasiri615@gmail.com>
2023-10-10 16:52:53 +0530

    changes made

--- tpg.sql
+++ tpg.sql
@@ -3,5 +3,7 @@ create database TPG;
 use tpg;
 show tables;
 show databases;
-
+select * from questions;
 drop table admin,questions,section,testpaper,subject;
+
+ALTER TABLE questions MODIFY COLUMN actual_qns VARCHAR(1000);
